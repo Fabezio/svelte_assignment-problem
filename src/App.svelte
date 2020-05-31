448e7fff0fa32b667431ddac74e60e0afb041221
fabezio <fabezio@outlook.fr>
2020-05-31 19:04:03 +0200

    basic resolved

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Tasks from './Tasks.svelte'
+  import Output from './Output.svelte'
+  export let name;
+  let heading = ""
+  
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+  <h2>You've got a job here: </h2>
+  <Tasks />
+
+	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+  <hr>
+  <h3>I've got the solution! </h3>
+  <input type="text" bind:value={heading} >
+  <Output {heading}  />
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Output.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let heading
+
+  $: {
+    console.log(`exclamation mark ${heading.includes('!') ? '' : 'not '}found`)
+  }
+
+</script>
+
+<h1 class:red={heading.includes('!')} >{heading}</h1>
+
+<style>
+.red {
+  color: red;
+}
+</style>
